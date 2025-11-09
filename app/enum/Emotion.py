dfe6ffac79afc2dfdd17c53b25e20ac0fc576f10
Nguyen Tien Phat <nguyentienphat@MacBook-Pro-cua-Nguyen.local>
2025-11-09 22:18:03 +0700

    Add VietNamese result

---
+++ app/enum/Emotion.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class Emotion(Enum):
+    neutral = "trung tính"
+    calm = "bình tĩnh"
+    happy = "vui vẻ"
+    sad = "buồn"
+    angry = "tức giận"
+    fearful = "sợ hãi"
+    disgust = "ghê tởm"
+    surprised = "ngạc nhiên"
